bcf8d075eb66f4905e2438eaf8097a59f6056a6d
Vladislav Fedorov <VEFedorov@glencore.onmicrosoft.com>
2023-08-09 13:37:44 +0600

    db

--- Weight.Program/Program.cs
+++ Weight.Program/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Weight.WeightDatabase;
+
+namespace Weight.Program;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //using (ApplicationContext db = new())
+        //{
+        //    db.Database.EnsureDeleted();
+        //    db.Database.EnsureCreated();
+        //    User user1 = new User { Name = "Tom", Age = 33 };
+        //    User user2 = new User { Name = "Alice", Age = 26 };
+        //
+        //    db.Users.AddRange(user1, user2);
+        //    db.SaveChanges();
+        //}
+
+        using (ApplicationContext db = new())
+        {
+            var users = db.Users.ToList();
+            Console.WriteLine("Users list:");
+            foreach (var u in users)
+            {
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+        }
+    }
+}
---
+++ Weight.Program/WeightDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Weight.WeightDatabase;
+
+public sealed class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True");
+    }
+}
