bbf2c1651c5007a9962ef552a177dc9f1c4efdfb
Vladislav Fedorov <VEFedorov@glencore.onmicrosoft.com>
2023-08-09 08:56:08 +0600

    Add project files.

---
+++ Weight.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Weight.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
